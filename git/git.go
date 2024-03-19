d8b8b3d8ba7dd235e9bad8db3c7b3c06ba01f056
Michael Lai <mlai@trucksmarter.com>
2024-03-19 10:48:11 -0400

    Print out unsorted stack

--- git/git.go
+++ git/git.go
@@ -3,8 +3,11 @@ package git
 import (
 	"fmt"
 	"os/exec"
+	"regexp"
 	"strings"
 )
+
+var refHeadRegexp = regexp.MustCompile(`^refs/heads/(.+)$`)
 
 func GetRecentHeads() ([]string, error) {
 	out, err := exec.Command("git", "for-each-ref", "--sort=-committerdate", "--count=50", "refs/heads/").Output()
@@ -15,7 +18,11 @@ func GetRecentHeads() ([]string, error) {
 	refs := make([]string, 0, len(lines))
 	for _, line := range lines {
 		parts := strings.Split(line, "\t")
-		refs = append(refs, parts[1])
+		nameMatch := refHeadRegexp.FindStringSubmatch(parts[1])
+		if nameMatch == nil {
+			return nil, fmt.Errorf("unexpected ref name: \"%s\"", parts[1])
+		}
+		refs = append(refs, nameMatch[1])
 	}
 	return refs, nil
 }
@@ -25,12 +32,18 @@ type Commit struct {
 	Message string
 }
 
+const mainBranch = "origin/main"
+
 func GetLogs(ref string) ([]Commit, error) {
-	out, err := exec.Command("git", "log", `--pretty=format:%H	%s`, "-n", "20", ref).Output()
+	out, err := exec.Command("git", "log", `--pretty=format:%H	%s`, fmt.Sprintf("%s..%s", mainBranch, ref)).Output()
 	if err != nil {
 		return nil, err
 	}
-	lines := strings.Split(strings.TrimSpace(string(out)), "\n")
+	trimmedOutput := strings.TrimSpace(string(out))
+	if trimmedOutput == "" {
+		return []Commit{}, nil
+	}
+	lines := strings.Split(trimmedOutput, "\n")
 	commits := make([]Commit, 0, len(lines))
 	for _, line := range lines {
 		parts := strings.Split(line, "\t")
@@ -41,4 +54,42 @@ func GetLogs(ref string) ([]Commit, error) {
 
 	}
 	return commits, nil
+}
+
+// Returns true if refA is an ancestor of refB
+func IsAncestor(refA string, refB string) (bool, error) {
+	cmd := exec.Command("git", "merge-base", "--is-ancestor", refA, refB)
+	if err := cmd.Run(); err != nil {
+		// --is-ancestor returns exit code 1 if the commit is not a part of main.
+		if exitError, ok := err.(*exec.ExitError); ok && exitError.ExitCode() == 1 {
+			return false, nil
+		}
+		return false, err
+	}
+	return true, nil
+}
+
+func MergeBase(refA string, refB string) (_ string, onMain bool, _ error) {
+	mergeBaseOut, err := exec.Command("git", "merge-base", refA, refB).Output()
+	if err != nil {
+		return "", false, err
+	}
+	hash := strings.TrimSpace(string(mergeBaseOut))
+	isOnMain, err := IsAncestor(hash, mainBranch)
+	if err != nil {
+		return hash, false, err
+	}
+	return hash, isOnMain, nil
+}
+
+func CurrentBranch() (string, error) {
+	out, err := exec.Command("git", "symbolic-ref", "HEAD").Output()
+	if err != nil {
+		return "", err
+	}
+	nameMatch := refHeadRegexp.FindStringSubmatch(strings.TrimSpace(string(out)))
+	if nameMatch == nil {
+		return "", fmt.Errorf("unexpected ref name: \"%s\"", string(out))
+	}
+	return nameMatch[1], nil
 }
--- main.go
+++ main.go
@@ -7,45 +7,134 @@ import (
 	"github.com/themichaellai/stacked-branches/git"
 )
 
-func getRecentHeads() error {
-	recentHeads, errs := git.GetRecentHeads()
-	if errs != nil {
-		return fmt.Errorf("error getting recent heads: %w", errs)
+const concurrency = 10
+
+func getMergeBases(targetRef string, refs []string) (refToMergeBase map[string]string, _ error) {
+	type result struct {
+		ref  string
+		base string
 	}
-	res := make(chan []git.Commit, 10)
+	workRefs := make(chan string, 10)
+	resChan := make(chan result)
 	errChan := make(chan error)
+	doneChan := make(chan struct{})
+	wg := sync.WaitGroup{}
 	go func() {
-		wg := sync.WaitGroup{}
-		for _, ref := range recentHeads[:50] {
-			wg.Add(1)
-			go func() {
-				defer wg.Done()
-				logs, err := git.GetLogs(ref)
-				if err != nil {
-					errChan <- fmt.Errorf("error getting logs for ref %s: %w", ref, err)
-				}
-				res <- logs
-			}()
+		for _, ref := range refs {
+			workRefs <- ref
 		}
-		wg.Wait()
-		close(res)
+		close(workRefs)
 	}()
+	for range concurrency {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for ref := range workRefs {
+				mergeBase, onMain, err := git.MergeBase(targetRef, ref)
+				if err != nil {
+					errChan <- err
+					return
+				}
+				if onMain {
+					continue
+				}
+				resChan <- result{ref, mergeBase}
+			}
+		}()
+	}
+	go func() {
+		wg.Wait()
+		close(doneChan)
+	}()
+	refToMergeBase = map[string]string{}
 	for {
 		select {
-		case el, more := <-res:
-			if !more {
-				fmt.Println("no more")
-				return nil
-			}
-			fmt.Println(el[0])
+		case el := <-resChan:
+			refToMergeBase[el.ref] = el.base
 		case err := <-errChan:
-			return err
+			return nil, err
+		case <-doneChan:
+			return refToMergeBase, nil
 		}
 	}
 }
 
+type Node struct {
+	Hash string
+	// Children are commits that are newer
+	Children []string
+	// Parent is the commit that is older
+	Parent string
+}
+
+//func getCandidateStack(logs map[string]([]git.Commit), targetRef string) ([]string, error) {
+//	// Build commit tree
+//	//hashTree := map[string]([]string){}
+//	//refToHead := map[string]string{}
+//	//for ref, log := range logs {
+//	//	fmt.Println(ref)
+//	//	refToHead[ref] = log[0].Hash
+//	//	for idx, commit := range log[:len(log)-1] {
+//	//		hashTree[commit.Hash] = append(hashTree[commit.Hash], log[idx+1].Hash)
+//	//	}
+//	//}
+//	tree := map[string]*Node{}
+//	for ref, log := range logs {
+//		fmt.Println(ref)
+//		for idx, commit := range log[:len(log)-1] {
+//			node := tree[commit.Hash]
+//			fmt.Printf("curr hash: %s\n", commit.Hash)
+//			if node == nil {
+//				node = &Node{Hash: commit.Hash}
+//				tree[commit.Hash] = node
+//			}
+//			if node.Parent != "" && node.Parent != log[idx+1].Hash {
+//				fmt.Printf("node.Parent: %s, log[idx+1].Hash: %s\n", node.Parent, log[idx+1].Hash)
+//				return nil, fmt.Errorf("foo")
+//			}
+//			node.Parent = log[idx+1].Hash
+//			if idx != 0 {
+//				node.Children = append(node.Children, log[idx-1].Hash)
+//			}
+//		}
+//	}
+//	for _, commit := range logs[targetRef] {
+//		fmt.Println(commit)
+//	}
+//
+//	// Find merge base for target ref with all other refs
+//	// Do this by commit hash, but in future can use commit message as well
+//	// Order refs with the highest number of common commits into stack
+//
+//	return nil, nil
+//}
+
+func getCandidateStack(targetRef string) ([]string, error) {
+	recentHeads, err := git.GetRecentHeads()
+	if err != nil {
+		return nil, fmt.Errorf("error getting recent heads: %w", err)
+	}
+	mergeBases, err := getMergeBases(targetRef, recentHeads)
+	if err != nil {
+		return nil, fmt.Errorf("error getting merge bases: %w", err)
+	}
+	res := make([]string, 0, len(mergeBases))
+	for ref := range mergeBases {
+		res = append(res, ref)
+	}
+	return res, nil
+}
+
 func run() error {
-	err := getRecentHeads()
+	currentBranch, err := git.CurrentBranch()
+	if err != nil {
+		return fmt.Errorf("could not get current branch: %w", err)
+	}
+	stack, err := getCandidateStack(currentBranch)
+	if err != nil {
+		return fmt.Errorf("error building stack: %w", err)
+	}
+	fmt.Printf("stack: %#v\n", stack)
 	return err
 }
 
